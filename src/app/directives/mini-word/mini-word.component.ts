bb7cff28bf859e279c9415fed8559bd7af61310a
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-23 16:59:47 +0200

    miniWord correction

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./mini-word.component.css']
 })
 export class MiniWordComponent {
-
+  color = '#FF1133';
+  size = 75;
+  font = 'garamond';
 }
