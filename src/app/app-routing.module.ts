5a8d127f5e07b6be878e92d7dd8bdf4b6a7bf91e
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-24 17:22:09 +0200

    correction exo Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { SecondComponent } from './components/second/second.component';
 import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 import { APP_ROUTES } from './config/app-routes.config';
 import { NF404Component } from './components/nf404/nf404.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: FirstComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'todo', component: TodoComponent },
   { path: 'word', component: MiniWordComponent },
   { path: 'color', component: ColorComponent },
+  { path: 'login', component: LoginComponent },
   { path: ':qqeChose', component: SecondComponent },
   { path: '**', component: NF404Component },
 ];
